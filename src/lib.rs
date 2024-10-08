d2d60aa9d085c6f279167c2e6fb67b4d1641c18f
Robert Garrett <robertgarrett404@gmail.com>
2024-10-08 12:56:59 -0500

    Demo of extracting data by iteration
    I've made a button that will call a function in the WASM module.
    Internally, this works as a kind of iterator -- it's not, for Rust
    lifetime annotation reasons, but it acts like one.
    
    I can move on to generating data and emitting that. Path tracer web
    port, when??

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,29 @@
 
 use wasm_bindgen::prelude::*;
 
+const PPM_DATA: &'static str = "one two three four five";
+
+// It's like an Iterator, but doesn't `impl Iterator ...`
+// I'm not certain I can export a Rust std::Iterator through
+// WASM, so I'm doing this nonsense.
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+struct ImageStepper {
+    idx: usize,
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+impl ImageStepper {
+    pub fn new() -> Self {
+        ImageStepper { idx: 0 }
+    }
+    
+    pub fn get_next(&mut self) -> Option<String> {
+        self.idx += 1;
+        let slice = PPM_DATA.get(self.idx..(self.idx+1))?;
+        return Some(String::from(slice))
+    }
+
+    pub fn has_more(&self) -> bool {
+        self.idx >= PPM_DATA.len()
+    }
 }
