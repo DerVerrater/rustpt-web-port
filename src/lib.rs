716a4d2a93ea2ad28d17725d238c095018eb7111
Robert Garrett <robertgarrett404@gmail.com>
2024-10-09 12:07:27 -0500

    Let Rust WASM module touch the canvas directly
    https://stackoverflow.com/questions/70309403/updating-html-canvas-imagedata-using-rust-webassembly
    
    Images can be posted into a <canvas> element by instantiating an
    ImageData object. The web-sys and js-sys crates expose these constructs
    to Rust, which means I can do basically everything there.
    
    The JavaScript module is just some startup code and the button callback
    registration. Onward to the rendering engine!
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch trunk
    # Your branch is up to date with 'github/trunk'.
    #
    # Changes to be committed:
    #	modified:   Cargo.toml
    #	modified:   index.html
    #	modified:   src/lib.rs
    #
    # Untracked files:
    #	Cargo.lock
    #

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,49 @@
 
-use wasm_bindgen::prelude::*;
+
+use wasm_bindgen::{prelude::*, Clamped};
+use web_sys::ImageData;
 
 const PPM_DATA: &'static str = include_str!("small.ppm");
 
-// It's like an Iterator, but doesn't `impl Iterator ...`
-// I'm not certain I can export a Rust std::Iterator through
-// WASM, so I'm doing this nonsense.
 #[wasm_bindgen]
-struct ImageStepper {
-    width: u32,
-    height: u32,
-    itr: std::str::Split<'static, &'static str>,
+pub async fn paint_image(canvas_name: String) -> Result<(), JsValue> {
+
+    let vec_bytes = ppm_to_u8(&PPM_DATA);
+    
+    let window = web_sys::window().unwrap();
+    let document = window.document().expect("Could not get document");
+    let canvas = document
+        .get_element_by_id(&canvas_name)
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+    let context = canvas
+        .get_context("2d")?
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
+    let clamped_buf: Clamped<&[u8]> = Clamped(&vec_bytes);
+    let image_data_temp = ImageData::new_with_u8_clamped_array_and_sh(clamped_buf, 80, 53)?;
+    context.put_image_data(&image_data_temp, 0.0, 0.0)?;
+    Ok(())
 }
 
-#[wasm_bindgen]
-impl ImageStepper {
-    pub fn new() -> Self {
-        let mut itr = PPM_DATA.split("\n");
-        // skip the first part. It's just the literal text "P3\n"They're metadata from the PPM file header.
-        let _ = itr.next();
-        let mut size = itr.next()
-            .unwrap()
-            .split(" ");
-        let width: u32 = size.next()
-            .unwrap()
-            .parse()
-            .unwrap();
-        let height: u32  = size.next()
-            .unwrap()
-            .parse()
-            .unwrap();
+fn ppm_to_u8<'input>(input: &'input str) -> Vec<u8> {
+    let mut ppm_iter = input.split("\n");
+    let _ = ppm_iter.next();    // consume "P3"
+    let _ = ppm_iter.next();    // consume width & height
+    let _ = ppm_iter.next();    // consume max value of each color (255)
 
-        ImageStepper { width, height, itr }
+    let mut bytes: Vec<u8> = Vec::new();
+    for line in ppm_iter {
+        for component in line.split(" ") {
+            if let Ok(value) = component.parse::<u8>() {
+                bytes.push(value);
+            }
+        }
+        bytes.push(255);
     }
-    
-    // I know that I'm loading an 80x80 image, but the program does not.
-    // Yay for magic numbers
-    pub fn width(&self) -> u32 { return self.width; }
-    pub fn height(&self) -> u32 { return self.height; }
-    
-    pub fn get_next(&mut self) -> Option<String> {
-        return Some(String::from(self.itr.next()?))
-    }
+    // the last line is empty. I'm not going to fix the logic to avoid
+    // pushing an extra alpha value, I'll just do this garbage.
+    // It's not supposed to be a text parser anyway. Fight me.
+    bytes.pop();
+    return bytes;
 }
